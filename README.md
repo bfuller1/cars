5988ddccef667c038f5a0dca10dc5b5304600255
bfuller1 <bfuller@chapman.edu>
2018-12-06 19:26:01 -0800

    Update

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# cars
+# What Cars.sh Does
+:octocat: :octocat: :octocat:
+
+## Enter a car
+* The function allows the user to enter in a car.
+ * When entering in a car, the program will ask you for three things:
+  * Year, Make, and Model
+
+## List old cars
+* The function allows you to display a list of all the old cars you have
+ * The function will update the list after you enter in a car
+ * The list is sorted by the year of the car.
+
+## Quitting the function
+* The program will say "Goodbye" and then you can exit.
+
+# :v:
+
+> XOXO
--- cars.sh
+++ cars.sh
@@ -1,12 +1,33 @@
 #! /bin/bash
 #cars.sh
 #Blake Fuller
-echo "Enter the number of the command you want to"
-echo "1 Enter a car"
-echo "2 List cars"
-echo "3 Quit"
-read CARS
-while echo $CARS | egrep -v "[1-3]$" > /dev/null 2>&1
+while true
 do
-	"1") echo "Enter the year of the car";;
+echo -e "Enter the number of the command you want to"
+echo -e "1 Enter a car"
+echo -e "2 List cars"
+echo -e "3 Quit"
+	read INPUT
+	case $INPUT in
+		1)
+			echo -e "Enter the year of the car"
+			read YEAR
+			echo -e "Enter the make of the car"
+			read MAKE
+			echo -e "Enter the model of the car"
+			read MODEL
+			echo -e $YEAR:$MAKE:$MODEL >> My_old_cars
+			;;
+		2)
+			echo -e "Current List of cars"
+			sort My_old_cars | tr ":" " "
+			;;
+		3)
+			echo -e "Goodbye"
+			break
+			;;
+		*)
+			echo -e "Input not valid"
+			;;
+	esac
 done
